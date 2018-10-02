d8da58562e50fa6131349e1cadc20e9f16fc8688
izzako <43785121+izzako@users.noreply.github.com>
2018-10-02 22:22:58 +0700

    Update hitungXOR.m

--- hitungXOR.m
+++ hitungXOR.m
@@ -1,3 +1,11 @@
 function res = hitungXOR(numA, numB, numC)
   %insert function here
+  
+  %  convert numA, numB, numC ke binernya somehow
+  %  lalu masukin binernya ke vektornya
+  %  vektor biner dari numA dinamakan BinA, dst..
+  
+  hasilXOR = vekbinA + vekbinB + vekbinC;
+  hasilXOR = mod(hasilXOR, 2);
+  
 end
